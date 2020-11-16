48a08530c507060f8945dc9496b3928c48cafe60
Sebastian_Milian <milianmejiaedgarsebastian@yahoo.com>
2020-11-16 07:53:49 -0600

    Calculos de numeros primos y coprimos, interfaz rsa

---
+++ CifradoRSA/Irsa.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CifradoRSA
+{
+    interface Irsa
+    {
+        public string GenerarLlaves(int P, int Q)
+        {
+            return "";
+        }
+        public static string CifradoRSA(int Key, int N, string Clave)
+        {
+            var cadena = string.Empty;
+            return cadena;
+        }
+
+        public static string DescifradoRSA(int Key, int N, string Clave)
+        {
+            var cadena = string.Empty;
+            return cadena;
+        }
+
+
+    }
+}
---
+++ CifradoRSA/Nprimos.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CifradoRSA
+{
+    class Nprimos
+    {
+        public static bool esNumeroPrimo(int n)
+        {
+            if (n > 1)
+            {
+                for (int i = 2; i < n; i++)
+                {
+                    if (n % i == 0)
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private static List<int> obtenerCoprimos(int n)
+        {
+            //Lista de numeros
+            List<int> numeros = new List<int>();
+
+            for (int i = 2; i < n; i++)
+            {
+                numeros.Add(i);
+            }
+
+            //Factorizar
+            List<int> factores = new List<int>();
+
+            if (n % 2 == 0) //si es un numero par
+            {
+                factores.Add(2);
+            }
+
+            for (int i = 3; i < n / 2; i += 2)
+            {
+                if (n % i == 0)
+                {
+                    factores.Add(i);
+                }
+            }
+
+         
+            return numeros;
+        }
+
+        
+
+    }
+}
